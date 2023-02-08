9e18fa64ff1a44b204b881b7e49ce13bc43d64c5
Tom Johansson <tom.johansson123@gmail.com>
2023-02-08 09:56:04 +0000

    first home route created

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const app = require("./app");
+const { PORT } = require("./config");
 console.log('Hello Hello COdespace');
+
+app.listen (PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+    });
